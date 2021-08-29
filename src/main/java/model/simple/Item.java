135fcca615d60ee12ba8e59a5af0f7f63701eca0
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-08-29 23:12:32 +0100

    Item class remodelling.

--- src/main/java/model/simple/Item.java
+++ src/main/java/model/simple/Item.java
@@ -1,11 +1,14 @@
 package model.simple;
 
 import model.simple.Bid;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.validation.constraints.Future;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Collections;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -16,6 +19,16 @@ public class Item {
     @Id
     @Column(name = "id", nullable = false)
     private Long id;
+
+    @NotNull
+    @Size (min = 2,
+    max = 255,
+    message = "Name is required, maximum 255 characters."
+    )
+    protected String name;
+
+    @Future
+    protected Date auctionEnd;
 
     private Set<Bid> bid = new HashSet<Bid>();
 
