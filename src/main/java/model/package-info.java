a2a08ab013a5e65a767836957cbe49269a8edff1
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-09-13 09:17:00 +0100

    refactoring.

--- src/main/java/model/package-info.java
+++ src/main/java/model/package-info.java
@@ -1,16 +1,17 @@
-package model;
+
 
 @org.hibernate.annotations.GenericGenerator(
         name = "ID_GENERATOR",
         strategy = "enhanced-sequence",
         parameters = {
-                org.hibernate.annotations.Parameter(
+                @org.hibernate.annotations.Parameter(
                         name = "sequence_name",
                         value = "JPWH_SEQUENCE"
                 ),
-                org.hibernate.annotations.Parameter(
+                @org.hibernate.annotations.Parameter(
                         name = "initial_value",
                         value = "1000"
                 )
         }
 )
+package model;
--- src/main/java/model/querying/package-info.java
+++ src/main/java/model/querying/package-info.java
@@ -1,10 +1,8 @@
-package model.querying;
-
 @org.hibernate.annotations.NamedQueries({
         @org.hibernate.annotations.NamedQuery(
                 name = "findItemsOrderByName",
                 query = "select i from Item i order by i .name asc"
-                )})
+                ),
 @org.hibernate.annotations.NamedQuery(
         name = "findItemBuyNowPriceGreaterThan",
         query = "select i from Item i where i .buyNowPrice > :price",
