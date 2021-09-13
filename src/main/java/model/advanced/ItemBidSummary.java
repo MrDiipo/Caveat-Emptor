66736b24ceab00ac31b7139b42776078a29c1842
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-09-13 09:37:38 +0100

    Executing custom SQL.

---
+++ src/main/java/model/advanced/ItemBidSummary.java
@@ -0,0 +1,26 @@
+package model.advanced;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@org.hibernate.annotations.Immutable
+@org.hibernate.annotations.Subselect(
+        value = "select i.ID as ITEMID, i.ITEM_NAME as NAME, " +
+                "count(b.ID) as NUMBEROFBIDS " +
+                "from ITEM i left outer join BID b on i.ID = b.ITEM_ID " +
+                "group by i.ID, i.ITEM_NAME"
+)
+@org.hibernate.annotations.Synchronize({"Item", "Bid"})
+public class ItemBidSummary {
+
+    @Id
+    protected Long itemId;
+
+    protected String name;
+
+    protected long numberOfBids;
+
+    public ItemBidSummary() {
+    }
+}
