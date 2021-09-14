6f1cf70d9589c11d9329d854468b6d64d0c1d501
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-09-14 21:33:53 +0100

    Domain updated part 3.

--- src/main/java/model/advanced/usertype/Item.java
+++ src/main/java/model/advanced/usertype/Item.java
@@ -1,4 +1,29 @@
 package model.advanced.usertype;
 
+import model.advanced.MonetaryAmount;
+
+import javax.persistence.Column;
+import javax.validation.constraints.NotNull;
+
 public class Item {
+
+    @NotNull
+    @org.hibernate.annotations.Type(
+            type = "monetary_amount_usd"
+    )
+    @org.hibernate.annotations.Columns(columns = {
+            @Column(name = "BUYNOWPRICE_AMOUNT"),
+            @Column(name = "BUYNOWPRICE_CURRENCY", length = 3)
+    })
+    protected MonetaryAmount buyNowPrice;
+
+    @NotNull
+    @org.hibernate.annotations.Type(
+            type = "monetary_amount_eur"
+    )
+    @org.hibernate.annotations.Columns(columns = {
+            @Column(name = "INITIALPRICE_AMOUNT"),
+            @Column(name = "INITIALPRICE_CURRENCY", length = 3)
+    })
+    protected MonetaryAmount initialPrice;
 }
